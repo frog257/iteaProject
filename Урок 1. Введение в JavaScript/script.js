0a2aa9b25e8483ea26e5950f9cc435f8732e7cb7
frog257 <frog25785@gmail.com>
2020-07-17 20:59:26 +0300

    initial

---
+++ Урок 1. Введение в JavaScript/script.js
@@ -0,0 +1,2 @@
+a = prompt('1'), b = prompt('2'), c = prompt('3');
+document.write('<table><tr><th>' + a + '</th><td>' + b + '</td><td>' + c + '</td></tr></table>');
---
+++ Урок 7. Особенности js/slider_06_07_2020/script/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.awesome-slider').slick({
+        autoplay: false,
+        autoplaySpeed: 1000,
+        slidesToShow: 2,
+        dots: true
+    });
+  });
